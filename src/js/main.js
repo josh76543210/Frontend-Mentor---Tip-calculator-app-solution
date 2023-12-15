18612d4fedefc4302520ab36bf37018acb9ac16c
Josh <joshmm16@gmail.com>
2023-12-15 11:59:13 -0800

    Initial commit with HTML;

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// import javascriptLogo from "../public/javascript.svg";
+// import viteLogo from "../public/vite.svg";
+// import { setupCounter } from "./counter.js";
+import "../css/reset.css";
+import "../css/style.css";
+import "../css/queries.css";
